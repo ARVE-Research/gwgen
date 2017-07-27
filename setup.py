295a575478d272062dfa9c7f2e800b8cd39c9389
Chilipp <p.sommer@posteo.de>
2017-07-27 14:55:02 +0200

    Minor changes

--- setup.py
+++ setup.py
@@ -59,6 +59,7 @@ def configuration(parent_package='', top_path=None):
 
     return config
 
+
 install_requires = ['f90nml', 'psyplot', 'scipy', 'sqlalchemy', 'psycopg2',
                     'statsmodels', 'docrep', 'model-organization', 'xarray',
                     'six']
