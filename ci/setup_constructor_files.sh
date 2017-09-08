6e777f4cd98e7a7968f72e82200504e92e0fae3b
Chilipp <p.sommer@posteo.de>
2017-09-08 10:04:05 +0200

    do not install gcc or sed via homebrew

--- ci/setup_constructor_files.sh
+++ ci/setup_constructor_files.sh
@@ -20,11 +20,13 @@ set -e
 
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
     BASH_RC=.bash_profile
+    INPLACE='-i ""'
 else
     BASH_RC=.bashrc
+    INPLACE='-i'
 fi
 
-sed -i "s/PYTHON_VERSION/${PYTHON_VERSION}*/; s/GWGEN_VERSION/${GWGEN_VERSION}/; s#GWGEN_FILE#${GWGEN_FILE}#; s#PSYPLOT_FILE#${PSYPLOT_FILE}#" ci/gwgen_conda/construct.yaml
-sed -i "s#PSY_SIMPLE_FILE#${PSY_SIMPLE_FILE}#; s#PSY_REG_FILE#${PSY_REG_FILE}#" ci/gwgen_conda/construct.yaml
-sed -i "s/<<<BASH_RC>>>/${BASH_RC}/" ci/gwgen_conda/post.sh
-sed -i "s#CREATION TIME#`date`#; s/PYTHON_VERSION/${PYTHON_VERSION}/; s/GWGEN_VERSION/${GWGEN_VERSION}/; s/CONSTRUCTOR VERSION/`constructor -V`/" ci/gwgen_conda/EULA.txt
+sed $INPLACE "s/PYTHON_VERSION/${PYTHON_VERSION}*/; s/GWGEN_VERSION/${GWGEN_VERSION}/; s#GWGEN_FILE#${GWGEN_FILE}#; s#PSYPLOT_FILE#${PSYPLOT_FILE}#" ci/gwgen_conda/construct.yaml
+sed $INPLACE "s#PSY_SIMPLE_FILE#${PSY_SIMPLE_FILE}#; s#PSY_REG_FILE#${PSY_REG_FILE}#" ci/gwgen_conda/construct.yaml
+sed $INPLACE "s/<<<BASH_RC>>>/${BASH_RC}/" ci/gwgen_conda/post.sh
+sed $INPLACE "s#CREATION TIME#`date`#; s/PYTHON_VERSION/${PYTHON_VERSION}/; s/GWGEN_VERSION/${GWGEN_VERSION}/; s/CONSTRUCTOR VERSION/`constructor -V`/" ci/gwgen_conda/EULA.txt
