6bd5334924ca332e93bd10052c4cb54428847a64
Chilipp <p.sommer@posteo.de>
2018-07-01 11:52:23 +0200

    Apply post install patch for noarch packages
    This commit uses the fix in https://github.com/conda/constructor/issues/86#issuecomment-358895352 to move noarch packages to the site-packages directory

--- ci/constructor-files/post_osx.sh
+++ ci/constructor-files/post_osx.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+# noarch PATCH
+# Explicitly move noarch packages into `lib/python?.?/site-packages` as a
+# workaround to [this issue][i86] with lack of `constructor` support for
+# `noarch` packages.
+#
+# [i86]: https://github.com/conda/constructor/issues/86#issuecomment-330863531
+if [[ -e site-packages ]]; then
+    for DIR in site-packages/*; do
+        if [[ -d $DIR ]]; then
+            mv $DIR $PREFIX/lib/python?.?/site-packages
+        else
+            filename=$(basename -- "$DIR")
+            extension="${filename##*.}"
+            if [[ $extension == 'py' ]]; then
+                mv $DIR $PREFIX/lib/python?.?/site-packages
+            fi
+        fi
+    done
+    rm -r site-packages
+fi
+# END noarch PATCH
+
 # script that is called after the installation of gwgen_conda to ask whether
 # an alias for gwgen shall be created
 
